9963f4d1a945d207e0b87d8a3342bf45bf437694
CR <csernikr@gmail.com>
2020-04-04 11:23:12 +0200

    Add full paramtreized initializeApp to sw-template.js

--- mws-server/bin/sw-template.js
+++ mws-server/bin/sw-template.js
@@ -230,29 +230,35 @@ function sendMessageToAllClients(message) {
 
 
 /**
- * 
- * Import and configure the Firebase SDK
- * 
- * These scripts are made available when the app is served or deployed on
- * Firebase Hosting
- * 
- * If you do not serve/host your project using Firebase Hosting see
- * https://firebase.google.com/docs/web/setup
+ * Here is is the code snippet to initialize Firebase Messaging in the Service
+ * Worker when your app is not hosted on Firebase Hosting.
  */
 
+// [START initialize_firebase_in_sw]
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
 importScripts('https://www.gstatic.com/firebasejs/7.12.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.12.0/firebase-messaging.js');
 
-// Initialize the Firebase app in the service worker by passing in the
-// messagingSenderId.
-firebase.initializeApp({'messagingSenderId': '99934082315'});
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
+firebase.initializeApp({
+    apiKey: "AIzaSyD7afX4U-1Mg6J5qHNPZIIH-5E_0DLv0cs",
+    authDomain: "mywindspot.firebaseapp.com",
+    databaseURL: "https://mywindspot.firebaseio.com",
+    projectId: "mywindspot",
+    storageBucket: "mywindspot.appspot.com",
+    messagingSenderId: "819092286463",
+    appId: "1:819092286463:web:04a0ab36ccf09b70"  
+});
 	  
 // Retrieve an instance of Firebase Messaging so that it can handle background
 // messages.
 const messaging = firebase.messaging();
+// [END initialize_firebase_in_sw]
+
 
 // If you would like to customize notifications that are received in the
 // background (Web app is closed or not in browser focus) then you should
